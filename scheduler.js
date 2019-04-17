e0f3c97927433ab7ba93f8ce141c449d8b15e524
AashitaP <harshupatwari@gmail.com>
2019-04-16 23:20:50 -0700

    added files

---
+++ scheduler.js
@@ -0,0 +1,24 @@
+var lib_info_scraper = require("./libraryinfo-scraper");
+var reslife_scraper = require("./reslife-scraper");
+
+var min_to_ms = 60000;
+var reslife_scraper_interval = 5 * min_to_ms;
+var lib_info_scraper_interval = 10080 * min_to_ms
+// console.log("I am doing my 5 minutes check");
+
+setInterval(function () {
+  console.log("I am doing my 1 week check");
+  lib_info_scraper.scrape();
+  // do your stuff here
+}, lib_info_scraper_interval); 
+
+setInterval(function () {
+  console.log("I am doing my 5 minutes check");
+  reslife_scraper.scrape();
+  // do your stuff here
+}, lib_info_scraper_interval);
+
+// setInterval(function () {
+//   console.log("I am doing my 10 minutes check");
+//   // do your stuff here
+// }, the_interval * 2);
